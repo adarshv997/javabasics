3c9fd4f2ed85b487c2ff0310c7ad465d256ffb80
adarshv997 <adarshv997@gmail.com>
2021-02-02 17:52:14 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args) {
+        String[] players = {"kholi", "sachin", "dhoni", "yuvi"};
+        System.out.println(players[0]);
+        for (String i : players) {
+            System.out.println(i);
+      }
+    }
+    
+}
---
+++ ifloop.java
@@ -0,0 +1,13 @@
+public class ifloop {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 58;
+        if (x > y) {
+          System.out.println("x is greater than y");
+        }  
+        else{
+            System.out.println("y is greater than x");
+
+        }
+      }
+}
